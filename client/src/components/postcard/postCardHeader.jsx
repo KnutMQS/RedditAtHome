17320559659252cec5413f21d6c7b103f7752bed
KnutMQS <knutmqs@gmail.com>
2025-11-12 09:53:11 +0100

    Changed <p> to <span>

--- client/src/components/postcard/postCardHeader.jsx
+++ client/src/components/postcard/postCardHeader.jsx
@@ -2,7 +2,7 @@ function PostCardHeader() {
   return (
     <>
       <div className="flex justify-between items-center sm:mx-auto px-5 sm:max-w-[50%] p-3">
-        <p>Username</p>
+        <span>Username</span>
         <time className="font-light text-xs opacity-60">14hr. ago</time>
       </div>
     </>
