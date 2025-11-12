b4f88643037930844fc2f317c7f7b874090ee089
KnutMQS <knutmqs@gmail.com>
2025-11-12 10:21:31 +0100

    Padding change

--- client/src/components/postcard/postCardHeader.jsx
+++ client/src/components/postcard/postCardHeader.jsx
@@ -1,7 +1,7 @@
 function PostCardHeader() {
   return (
     <>
-      <div className="flex justify-between items-center sm:mx-auto px-5 sm:max-w-[50%] p-3">
+      <div className="flex justify-between items-center sm:mx-auto sm:max-w-[50%] px-3 py-2 ">
         <span>Username</span>
         <time className="font-light text-xs opacity-60">14hr. ago</time>
       </div>
