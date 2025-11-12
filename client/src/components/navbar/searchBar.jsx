f053539eb6b63765eedda41fc964393e2e754b2c
KnutMQS <knutmqs@gmail.com>
2025-11-12 08:23:45 +0100

    color update

--- client/src/components/navbar/searchBar.jsx
+++ client/src/components/navbar/searchBar.jsx
@@ -2,11 +2,11 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchBar = () => {
   return (
-    <div className="flex flex-1 max-w-[40%] mx-2">
+    <div className="flex flex-1 max-w-[40%]">
       <input
         type="text"
         placeholder="Search..."
-        className="bg-gray-700 border rounded-l border-[var(--color-border)] px-1 sm:px-2 py-1 flex-1 text-sm sm:text-base"
+        className="bg-[var(--color-search)] border rounded-l border-[var(--color-border)] px-1 sm:px-2 py-1 flex-1 text-sm sm:text-base"
       />
       <button className="border border-[var(--color-border)] bg-[var(--color-button-bg)] rounded-r px-1 sm:px-2 py-1 flex items-center space-x-1 sm:space-x-2">
         <FaSearch />
