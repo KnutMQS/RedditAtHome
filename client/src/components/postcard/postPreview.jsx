9e69f6daf43a08df6d11b246892d942da91f194e
KnutMQS <knutmqs@gmail.com>
2025-11-17 08:33:45 +0100

    Fix image import and enhance PostPreview component layout

--- client/src/components/postcard/postPreview.jsx
+++ client/src/components/postcard/postPreview.jsx
@@ -1,10 +1,14 @@
-import image from "../../assets/kurtkårejohnny.png";
+import image from "../../assets/kurtkårejohnny.jpg";
 
 function PostPreview() {
   return (
     <div className="flex-col sm:max-w-[50%] border border-t-0 border-[var(--color-divider)] px-3 pb-2 sm:mx-auto">
       <h1 className="pb-2">Title: Rookie mistakes in progress</h1>
-      <img src={image} alt="" className="rounded-xl object-contain mx-auto" />
+      <img
+        src={image}
+        alt=""
+        className="rounded-xl h-full w-full object-contain mx-auto"
+      />
     </div>
   );
 }
