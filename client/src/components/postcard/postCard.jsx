db6837f09e99ab0e05c25fecd4865f9760b28c90
KnutMQS <knutmqs@gmail.com>
2025-11-20 10:50:02 +0100

    Refactor PostCard component structure by replacing fragment with article and added PostCardContent and PostCardFooter

--- client/src/components/postcard/postCard.jsx
+++ client/src/components/postcard/postCard.jsx
@@ -1,12 +1,14 @@
 import PostCardHeader from "./postCardHeader";
-import PostPreview from "./postPreview";
+import PostCardContent from "./postCardContent";
+import PostCardFooter from "./postCardFooter";
 
 function PostCard() {
   return (
-    <>
+    <article className=" w-full max-w-2xl mx-auto border border-t-0 border-[var(--color-divider)] pt-0 px-4 py-3 overflow-hidden">
       <PostCardHeader />
-      <PostPreview />
-    </>
+      <PostCardContent />
+      <PostCardFooter />
+    </article>
   );
 }
 
