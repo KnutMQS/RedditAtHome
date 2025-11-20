1649d6c52a6c8fb1a8fdf3ea599b789e60c62a74
KnutMQS <knutmqs@gmail.com>
2025-11-20 10:50:48 +0100

    Refactor PostCardHeader component layout and improve time element styling

--- client/src/components/postcard/postCardHeader.jsx
+++ client/src/components/postcard/postCardHeader.jsx
@@ -1,8 +1,10 @@
 function PostCardHeader() {
   return (
-    <div className="flex justify-between items-center border border-t-0 border-b-0 border-[var(--color-divider)] sm:mx-auto sm:max-w-[50%] px-3 py-2 ">
+    <div className="flex justify-between items-center pr-3 py-2 ">
       <span>Username</span>
-      <time className="font-light text-xs opacity-60">14hr. ago</time>
+      <time className=" justify-end-safe font-light text-xs opacity-60">
+        14hr. ago
+      </time>
     </div>
   );
 }
