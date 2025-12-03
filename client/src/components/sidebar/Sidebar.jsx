3aa151c53856fa8eba752c4d63c5fe878e09a5d7
KnutMQS <knutmqs@gmail.com>
2025-12-03 08:42:42 +0100

    Hide Sidebar on smaller screens for better responsiveness

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 function Sidebar() {
   return (
-    <nav className="border-border fixed top-auto flex h-screen w-3xs flex-col space-y-2 border-r-2">
+    <nav className="border-border fixed top-auto hidden h-screen w-3xs flex-col space-y-2 border-r-2 lg:flex">
       <div className="border-border flex justify-center border-b-2 py-5">
         <ul className="space-y-5">
           <li className="border-border border-b px-2">
