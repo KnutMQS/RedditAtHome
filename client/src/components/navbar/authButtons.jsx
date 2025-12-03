c423f86edf45944db39b2405f79d2b086cd8d7a4
KnutMQS <knutmqs@gmail.com>
2025-12-03 09:18:23 +0100

    More consistent styling

--- client/src/components/navbar/authButtons.jsx
+++ client/src/components/navbar/authButtons.jsx
@@ -3,11 +3,11 @@ import { FaUser, FaSignInAlt } from "react-icons/fa";
 const AuthButtons = () => {
   return (
     <div className="hidden items-center lg:flex lg:space-x-2 lg:px-2">
-      <button className="flex items-center space-x-1 rounded border border-[var(--color-border)] bg-[var(--color-button-bg)] px-1 py-1 hover:invert">
+      <button className="border-border bg-button-bg flex items-center space-x-1 rounded border px-1 py-1 hover:invert">
         <FaUser />
         <span>Sign In</span>
       </button>
-      <button className="flex items-center space-x-1 rounded border border-[var(--color-border)] bg-[var(--color-button-bg)] px-1 py-1 hover:invert">
+      <button className="border-border bg-button-bg flex items-center space-x-1 rounded border px-1 py-1 hover:invert">
         <FaSignInAlt />
         <span>Register</span>
       </button>
