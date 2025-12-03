6bab714727da1c1ddc811dd0f237605dfe17854c
KnutMQS <knutmqs@gmail.com>
2025-12-03 09:21:51 +0100

    Refactor SearchBar component for improved styling consistency

--- client/src/components/navbar/searchBar.jsx
+++ client/src/components/navbar/searchBar.jsx
@@ -6,9 +6,9 @@ const SearchBar = () => {
       <input
         type="text"
         placeholder="Search..."
-        className="flex-1 rounded-l border border-[var(--color-border)] bg-[var(--color-search)] px-1 py-1 text-base"
+        className="border-border bg-search flex-1 rounded-l border px-1 py-1 text-base"
       />
-      <button className="flex items-center space-x-1 rounded-r border border-[var(--color-border)] bg-[var(--color-button-bg)] px-2.5 hover:invert">
+      <button className="border-border bg-button-bg flex items-center space-x-1 rounded-r border px-2.5 hover:invert">
         <FaSearch />
         <span className="hidden lg:block">Search</span>
       </button>
