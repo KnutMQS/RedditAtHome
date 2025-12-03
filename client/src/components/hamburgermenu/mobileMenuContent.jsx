daa4f1de20895101a159f66ec31586f2dfd0b3e1
KnutMQS <knutmqs@gmail.com>
2025-12-03 08:42:23 +0100

    fixed positioning issue. maybe?

--- client/src/components/hamburgermenu/mobileMenuContent.jsx
+++ client/src/components/hamburgermenu/mobileMenuContent.jsx
@@ -1,7 +1,7 @@
 function MobileMenuContent({ isOpen }) {
   return (
     <div
-      className={`border-border bg-bg absolute top-full right-0 z-50 mt-[-1px] min-w-48 border p-4 shadow-lg transition-all duration-100 ${
+      className={`border-border bg-bg absolute top-full right-0 z-50 mt-[-1px] mr-1 min-w-48 border p-4 shadow-lg transition-all duration-100 ${
         isOpen
           ? "after:bg-bg pointer-events-auto -top-px scale-100 rounded-tl rounded-b opacity-100 after:absolute after:top-0 after:right-0 after:h-px after:w-9 after:content-['']"
           : "pointer-events-none top-full scale-95 rounded opacity-0"
